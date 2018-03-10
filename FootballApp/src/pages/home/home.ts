7192869eb8c02d77b83ffa863af2c1055e664147
aefiq96 <aefiq96@gmail.com>
2018-03-10 20:50:59 +0000

    connecting to mysql and displaying data

--- FootballApp/src/pages/home/home.ts
+++ FootballApp/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,23 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http) {
 
+  }
+
+  //created a new variable of type any 
+  users:any;
+  //created a method which loads all the values from mysql in the webpage 
+  loadData(){
+
+    this.http.get('http://localhost/test.php').map(Data => Data.json()).subscribe(Data => {
+      //this assigns 
+      this.users = Data;
+      console.log(this.users);
+
+    },(err) =>{
+      alert("failed loading data");
+    });
   }
 
 }
